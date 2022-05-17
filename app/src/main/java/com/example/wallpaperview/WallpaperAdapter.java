e841c020cb48766c2019c6bece5021752c01aa37
omi-72 <jannat.omi722@gmail.com>
2022-05-17 23:51:34 +0600

    Adapter with RecyclerView

---
+++ app/src/main/java/com/example/wallpaperview/WallpaperAdapter.java
@@ -0,0 +1,24 @@
+package com.example.wallpaperview;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperViewHolder> {
+    @NonNull
+    @Override
+    public WallpaperViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WallpaperViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
