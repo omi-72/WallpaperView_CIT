aac7c44fc350953ec67eeb0a356964f5dec3405a
omi-72 <jannat.omi722@gmail.com>
2022-05-17 22:50:08 +0600

    create constractor or getter&Shatter

--- app/src/main/java/com/example/wallpaperview/Wallpaper.java
+++ app/src/main/java/com/example/wallpaperview/Wallpaper.java
@@ -3,4 +3,25 @@ package com.example.wallpaperview;
 public class Wallpaper {
     private String url;
     private String name;
+
+    public Wallpaper(String url, String name) {
+        this.url = url;
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
