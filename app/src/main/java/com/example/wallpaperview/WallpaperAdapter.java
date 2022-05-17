f30e3de6e424397d60ea0c056eb9098be28569ee
omi-72 <jannat.omi722@gmail.com>
2022-05-17 23:59:51 +0600

    adapter coding viewHolder

--- app/src/main/java/com/example/wallpaperview/WallpaperAdapter.java
+++ app/src/main/java/com/example/wallpaperview/WallpaperAdapter.java
@@ -1,15 +1,33 @@
 package com.example.wallpaperview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperViewHolder> {
+
+    ArrayList<Wallpaper>wallpapers;
+    Context context;
+
+    public WallpaperAdapter(ArrayList<Wallpaper> wallpapers, Context context) {
+        this.wallpapers = wallpapers;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public WallpaperViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(context).inflate(R.layout.item_wallpaper,parent,false);
+
+
+        return new WallpaperViewHolder(view);
     }
 
     @Override
