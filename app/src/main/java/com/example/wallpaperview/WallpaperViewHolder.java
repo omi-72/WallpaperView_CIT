15260294d94ab76b90fca3aee7041728eb3cd2a7
omi-72 <jannat.omi722@gmail.com>
2022-05-17 23:47:46 +0600

    adding ViewHolder code

---
+++ app/src/main/java/com/example/wallpaperview/WallpaperViewHolder.java
@@ -0,0 +1,22 @@
+package com.example.wallpaperview;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class WallpaperViewHolder extends RecyclerView.ViewHolder {
+
+    public ImageView imageView;
+    public TextView nameText;
+
+    public WallpaperViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        imageView= itemView.findViewById(R.id.wallpaper);
+        nameText= itemView.findViewById(R.id.wallName);
+
+    }
+}
